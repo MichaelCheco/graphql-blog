5b7b6243e05123638f37b62248570611a20a5265
Michael Checo <checomichael2@gmail.com>
2019-01-01 21:34:48 -0500

    implemented resolvers using Prisma

--- src/index.js
+++ src/index.js
@@ -5,40 +5,52 @@ const posts = []
 
 const resolvers = {
   Query: {
-    description: () => `This is the API of a blogging application`,
-    posts: () => posts,
-    post: (parent, args) => posts.find(post => post.id === args.id)
+      posts(parent, args, ctx, info) {
+          return ctx.db.query.posts({}, info)
+      },
+      post(parent, args, ctx, info) {
+          return ctx.db.query.post({ where: { id: args.id } }, info)
+      }
   },
   Mutation: {
-      createDraft: (parent, args) => {
-          const post = {
-              id: `post_${idCount++}`,
-              title: args.title,
-              content: args.content,
-              published: false
-          }
-          posts.push(post)
-          return post
+      createDraft(parent, { title, content }, ctx, info) {
+          return ctx.db.mutation.createPost(
+              {
+                  data: {
+                      title,
+                      content
+                  },
+              },
+              info,
+          )
       },
-      deletePost: (parent, args) => {
-          const postIndex = posts.findIndex(post => post.id === args.id)
-          if ( postIndex > -1) {
-              const deleted = posts.splice(postIndex, 1)
-              return deleted[0]
-          }
-          return null
+      deletePost(parent, { id }, ctx, info) {
+          return ctx.db.mutstion.deletePost({ where: { id } }, info)
       },
-      publish: (parent, args) => {
-        const postIndex = posts.findIndex(post => post.id === args.id)
-        posts[postIndex].published = true;
-        return posts[postIndex]
-      }
-  }
+      publish(parent, { id }, ctx, info) {
+          return ctx.db.mutation.updatePost(
+              {
+                  where: { id },
+                  data: { published: true},
+              },
+              info,
+          )
+      },
+  },
 };
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/michael-a-checo/database/dev',
+            secret: 'secret',
+            debug: true
+        })
+    })
 })
 
 server.start(() => console.log(`The server is running on http://localhost:4000`))
