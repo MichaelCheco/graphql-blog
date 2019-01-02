2740b32e4ee93f6f100fcead073235363b0582c7
Michael Checo <checomichael2@gmail.com>
2019-01-02 17:02:09 -0500

    Mutation resolvers

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,60 +1,53 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { hash, compare } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
 const { APP_SECRET, getUserId } = require("../utils");
 
-async function signup(parent, args, ctx, info) {
-    const password = await bcrypt.hash(args.password, 10)
-    const user = await ctx.db.mutation.createUser({
-    data: {name: args.name, email: args.email,  password}
-    })
-    const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
+const Mutation = {
+  signup: async (parent, { name, email, password }, context) => {
+    const hashedPassword = await hash(password, 10);
+    const user = await context.prisma.createUser({
+      name,
+      email,
+      password: hashedPassword
+    });
     return {
-        user,
-        token
+      token: sign({ userId: user.id }, APP_SECRET),
+      user
+    };
+  },
+  login: async (parent, { email, password }, context) => {
+    const user = await context.prisma.user({ email });
+    if (!user) {
+      throw new Error(`No user found for email: ${email}`);
     }
-}
-
-async function login(parent, args, ctx, info) {
-    const user = await ctx.db.user( { email: args.email } );
-    if(!user) {
-        throw new Error('no such user found')
+    const passwordValid = await compare(password, user.password);
+    if (!passwordValid) {
+      throw new Error("Invalid password");
     }
-    const valid = await bcrypt.compare(args.password, user.password)
-    if(!valid) {
-        throw new Error('invalid password')
-    }
-    const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
     return {
-        user,
-        token
-    }
-}
-function createDraft(parent, args, ctx, info) {
-    const userId = getUserId(ctx)
-    return ctx.db.mutation.createPost({
-        title: args.title,
-        content: args.content,
-        postedBy: { connect: {id: userId }}
-    }
-    )
-}
-function deletePost(parent, {  id }, ctx, info) {
-    return ctx.db.mutation.deletePost({  where:  { id } },  info)
-}
-
-function publish(parent,  {  id },  ctx, info) {
-    return ctx.db.mutation.updatePost({
-        where: { id },
-        data: { published: true}
-    }, info)
-}
+      token: sign({ userId: user.id }, APP_SECRET),
+      user
+    };
+  },
+  createDraft: async (parent, { title, content}, context) => {
+      const userId = getUserId(context)
+      return context.prisma.createPost({
+          title,
+          content,
+          author: { connect: { id: userId } }
+      })
+  },
+  deletePost: async (parent, { id }, context) => {
+      return context.prisma.deletePost({ id })
+  },
+  publish: async (parent, { id }, context) => {
+      return context.prisma.updatePost({
+          where: { id },
+          data: { published: true }
+      })
+  }
+};
 
 module.exports = {
-    signup,
-    login,
-    createDraft,
-    deletePost,
-    publish
+    Mutation
 }
