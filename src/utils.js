2dde44b3b48b6facb0c07b93eeb4eb69c2add2af
Michael Checo <checomichael2@gmail.com>
2019-01-02 16:45:14 -0500

    utils

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,20 @@
-const jwt = require('jsonwebtoken')
+const { verify } = require('jsonwebtoken')
+
 const APP_SECRET = 'GraphQL'
+
+// class AuthError extends Error {
+//     constructor() {
+//         super('Not Authorized')
+//     }
+// }
 
 function getUserId(context) {
     const Authorization = context.request.get('Authorization')
     if(Authorization) {
         const token = Authorization.replace('Bearer ', '')
-        const { userId } = jwt.verify(token, APP_SECRET)
-        return userId
+        const verifiedToken = verify(token, APP_SECRET)
+
+        return verifiedToken && verifiedToken.userId
     }
     throw new Error('Not authenticated')
 }
