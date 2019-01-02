71d6d7f70606c1c0dd0ede838f5f6391e2f652ad
Michael Checo <checomichael2@gmail.com>
2019-01-02 16:38:43 -0500

    update index file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const { GraphQLServer } = require("graphql-yoga");
 const  { prisma } = require('./generated/prisma.graphql')
-const Query = require('./resolvers/Query') 
-const Mutation = require('./resolvers/Mutation') 
-const resolvers = {
-    Query,
-    Mutation
-};
-
+const { resolvers } = require('./resolvers')
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: req => ({
-        ...req,
+    context: request => ({
+        ...request,
       prisma
     })
 })
